e5083cfcb7f6d0d277698fc9584e93ea010e1024
zilvinasblinovas <blinovaszilvinas@gmail.com>
2025-12-30 07:21:32 +0200

    chore: remove comment regarding user data fetching suggestion in AuthContext.

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -10,7 +10,7 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
-      setUser({ token }); // Galima papildyti užklausa į /users/me gauti vardui
+      setUser({ token });
     }
     setLoading(false);
   }, []);
